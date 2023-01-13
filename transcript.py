d09ac071a229b74403cf690b42aa18ecfafde046
JCardd <111624071+JCardd@users.noreply.github.com>
2023-01-13 05:46:53 -0500

    Implemented-error-handling
    Added error-handling to handle invalid URLs or videos without transcripts.

--- transcript.py
+++ transcript.py
@@ -2,27 +2,38 @@ import argparse
 import youtube_transcript_api
 
 def get_transcript(url):
-    # Extract the video id from the URL
-    video_id = url.split("v=")[1]
+    try:
+        # From the URL, fetch the video ID
+        video_id = url.split("v=")[1]
 
-    # Use the youtube_transcript_api library to fetch the transcript
-    transcript = youtube_transcript_api.YouTubeTranscriptApi.get_transcript(video_id)
-   
-    # Extract the text from the transcript
-    text = ""
-    for line in transcript:
-        text += line['text'] + '\n'
+        # Fetch the transcript using the YouTube transcript api
+        transcript = youtube_transcript_api.YouTubeTranscriptApi.get_transcript(video_id)
 
-    return text
+        # Extract the text from the transcript
+        text = ""
+        for line in transcript:
+            text += line['text'] + '\n'
+
+        return text
+    except youtube_transcript_api.CouldNotRetrieveTranscript:
+        print(f"Error: Could not retrieve transcript for video {video_id}")
+        return None
+    except IndexError:
+        print("Error: Invalid URL")
+        return None
 
 # Parse the command line arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("url", help="the YouTube URL of the video")
 args = parser.parse_args()
 
-# Fetch the transcript
+# Get the transcript
 transcript = get_transcript(args.url)
 
-# Write the transcript to a file
-with open("transcript.txt", "w") as f:
-    f.write(transcript)
+# Write the transcript to the file
+if transcript:
+    with open("transcript.txt", "w") as f:
+        f.write(transcript)
+    print(f"Transcript for video {video_id} written to transcript.txt")
+else:
+    print("Transcript not written to file")
